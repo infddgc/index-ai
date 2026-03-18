360421d5f00b8913d43a273c4a9fbdf3f7c0567d
infddgc <mikerrrr24@gmail.com>
2026-03-18 21:21:43 +0400

    Create confug.js

---
+++ config/confug.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+    API_KEY: "tgp_v1_dF02yOUYxQwHn7q_yxVxnvjvl393Q07VJG5xRgjf5rk", // 
+    MODEL: "meta-llama/Meta-Llama-3-8B-Instruct",
+    MAX_TOKENS: 400,
+    DEFAULT_THEME: "dark"
+};
