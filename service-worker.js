e43545b1a438fac8acebe14d99f18c2fdf53b00c
infddgc <mikerrrr24@gmail.com>
2026-03-18 21:33:31 +0400

    Update cache name and caching logic in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-const CACHE = "index-cache-v1";
+const CACHE_NAME = "index-ai-cache-v2";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style/main.css",
+  "/style/chat.css",
+  "/style/mobile.css",
+  "/js/app.js",
+  "/js/auth.js",
+  "/js/chat.js",
+  "/js/storage.js"
+];
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js"
-      ]);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
